4cf86b3ba1a943e1f4e348484dc882eef42aaf05
patelkenil26 <patelkenil2353@gmail.com>
2025-11-29 17:50:16 +0530

    update frontend link

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://expence-tracker-beta-gray.vercel.app/",
+    origin: "https://expence-tracker-beta-gray.vercel.app",
     credentials: true,
   })
 );
